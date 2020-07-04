25d21e27f54d77d9842e7b98a73792ca9500d11e
PasanMahesha <Pmahesha.hera@gmail.com>
2020-07-04 17:51:37 +0530

    Book-now_popup

--- src/app/doctor-book/doctor-book.component.ts
+++ src/app/doctor-book/doctor-book.component.ts
@@ -14,6 +14,7 @@ export class DoctorBookComponent implements OnInit {
   document: any;
   id: any;
   data: any;
+  
   private routeSub: Subscription;
   constructor(
     public auth: AuthService,
