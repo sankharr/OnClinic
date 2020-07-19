1892690afbc93d8a60939f58be81e45f6e1c7e54
ThiliniAsmada <thilinida96@gmail.com>
2020-07-19 22:38:50 +0530

    patient payment backed created

--- src/app/layouts/patients/patient-payments/patient-payments.component.ts
+++ src/app/layouts/patients/patient-payments/patient-payments.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/firestore';
 
 @Component({
   selector: 'app-patient-payments',
@@ -6,10 +7,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./patient-payments.component.css']
 })
 export class PatientPaymentsComponent implements OnInit {
+  uid: any;
+  userData: any;
+  result: any[];
 
-  constructor() { }
+  constructor(
+    private db: AngularFirestore,
+  ) {this.uid=localStorage.getItem("uid"); }
 
   ngOnInit(): void {
+    this.db.collection('Users').doc(this.uid).valueChanges()
+      .subscribe(output => {
+        this.userData = output;
+        console.log("userData of patients - ", this.userData);
+        this.db.collection('Appointments', ref => ref.where("status", "==", "Success").where("patientID", "==", this.userData.patientID).orderBy("appointmentDate")).valueChanges()
+          .subscribe(output3 => {
+            this.result = output3;
+            console.log("Get All Past Channelings - ", this.result);
+          })
+      })
   }
 
 }
