3a0bb7a14ac6398b26c547060584e63f9ec12efe
ThiliniAsmada <thilinida96@gmail.com>
2020-07-28 16:13:16 +0530

    upload inquiry backend created

--- src/app/layouts/patients/patient-payments/patient-payments.component.ts
+++ src/app/layouts/patients/patient-payments/patient-payments.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AngularFirestore } from '@angular/fire/firestore';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { DatePipe } from '@angular/common';
 
 @Component({
   selector: 'app-patient-payments',
@@ -10,10 +12,16 @@ export class PatientPaymentsComponent implements OnInit {
   uid: any;
   userData: any;
   result: any[];
-
+  inquiryForm: FormGroup;
+  last_InquiryID: any;
+  sys_variableData: any;
+  selectedInquiryAppointmentData: any;
+  inquirySubmitted: boolean = false;
   constructor(
     private db: AngularFirestore,
-  ) {this.uid=localStorage.getItem("uid"); }
+    private _formbuilder: FormBuilder,
+    private datePipe: DatePipe
+  ) { this.uid = localStorage.getItem("uid"); }
 
   ngOnInit(): void {
     this.db.collection('Users').doc(this.uid).valueChanges()
@@ -26,6 +34,49 @@ export class PatientPaymentsComponent implements OnInit {
             console.log("Get All Past Channelings - ", this.result);
           })
       })
+    this.inquiryForm = this._formbuilder.group({
+      Reason: ["", Validators.required],
+    });
   }
 
+  loadInquiryData(appoID) {
+    this.db.collection("Appointments").doc(appoID).valueChanges()
+      .subscribe(output => {
+        this.selectedInquiryAppointmentData = output
+      })
+      this.db.collection("system_variables").doc("system_variables").valueChanges()
+      .subscribe(output4 => {
+        this.sys_variableData = output4;
+      })
+
+  }
+
+  submit() {
+   
+    var data = {
+      inquiryID: this.sys_variableData.last_InquiryID + 1,
+      appointmentID: this.selectedInquiryAppointmentData.appointmentID,
+      patientID: this.selectedInquiryAppointmentData.patientID,
+      patientName: this.selectedInquiryAppointmentData.patientName,
+      doctorID: this.selectedInquiryAppointmentData.doctorID,
+      doctorName: this.selectedInquiryAppointmentData.doctorName,
+      appointmentDate: (this.selectedInquiryAppointmentData.appointmentDate),
+      inquiryDate: new Date(),
+      totalFee: this.selectedInquiryAppointmentData.totalFee,
+      appointmentNo: this.selectedInquiryAppointmentData.appointmentNo,
+      doctorSpeciality: this.selectedInquiryAppointmentData.doctorSpeciality,
+      reason: this.inquiryForm.controls["Reason"].value,
+      status: 'Unread'
+    }
+    this.db.collection("Inquries").add(data)
+      .then(() => {
+        this.inquirySubmitted = true;
+        setTimeout(() => {this.inquirySubmitted=false}, 1000)
+        this.db.collection("system_variables").doc("system_variables").update({
+          last_InquiryID:data.inquiryID,
+        })
+      });
+    console.log("data to be saved from submit() - ", data);
+  }
 }
+
