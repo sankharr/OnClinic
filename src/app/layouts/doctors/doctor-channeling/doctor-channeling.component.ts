0a9184cf72588cda7896c2034c3185c8c5644855
MaheemaPavithrani <maheemapavithrani@gmail.com>
2020-08-21 21:07:09 +0530

    filter appointments for user

--- src/app/layouts/doctors/doctor-channeling/doctor-channeling.component.ts
+++ src/app/layouts/doctors/doctor-channeling/doctor-channeling.component.ts
@@ -1,15 +1,62 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { AngularFirestore } from "@angular/fire/firestore";
+import * as _ from "lodash";
+import { group } from "console";
 
 @Component({
-  selector: 'app-doctor-channeling',
-  templateUrl: './doctor-channeling.component.html',
-  styleUrls: ['./doctor-channeling.component.css']
+  selector: "app-doctor-channeling",
+  templateUrl: "./doctor-channeling.component.html",
+  styleUrls: ["./doctor-channeling.component.css"],
 })
 export class DoctorChannelingComponent implements OnInit {
+  result: any;
+  userId: any;
+  app: any;
+  // doctorId : string;
 
-  constructor() { }
+  constructor(private db: AngularFirestore) {}
 
   ngOnInit(): void {
+    this.userId = localStorage.getItem("uid");
+    this.db
+      .collection("Appointments")
+      .valueChanges()
+      .subscribe((output) => {
+        this.result = output;
+        // console.log(this.result);
+        this.test(this.result);
+        this.getUserState();
+      });
   }
 
+  getUserState() {
+    //   this.userId = localStorage.getItem("uid");
+    //   this.db.collection("Users").doc(this.userId).valueChanges()
+    //   .subscribe(res=>{
+    //     // console.log(res)
+    //     this.doctorId = res["doctorID"];
+    //     console.log(this.doctorId)
+    // })
+  }
+
+  test(data) {
+    // this.userId = localStorage.getItem("uid");
+    // console.log(this.userId);
+    // var id = toString(this.userId);
+    this.userId = localStorage.getItem("uid");
+    this.db
+      .collection("Users")
+      .doc(this.userId)
+      .valueChanges()
+      .subscribe((res) => {
+        // console.log(res)
+        var doctorId = res["doctorID"];
+        console.log(doctorId);
+        var grouped = _.mapValues(_.groupBy(data, "doctorID"), (clist) =>
+          clist.map((data) => _.omit(data, "doctorID"))
+        );
+        this.app = grouped[doctorId];
+        console.log(this.app);
+      });
+  }
 }
