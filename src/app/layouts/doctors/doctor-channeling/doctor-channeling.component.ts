6781a29f672b621999d9382f0136af200c489439
MaheemaPavithrani <maheemapavithrani@gmail.com>
2020-10-06 17:41:26 +0530

    waiting room completed

--- src/app/layouts/doctors/doctor-channeling/doctor-channeling.component.ts
+++ src/app/layouts/doctors/doctor-channeling/doctor-channeling.component.ts
@@ -1,9 +1,11 @@
 import { Component, OnInit } from "@angular/core";
 import { AngularFirestore } from "@angular/fire/firestore";
+import { DatePipe } from "@angular/common";
 import * as _ from "lodash";
 import { group } from "console";
 import { Timestamp } from "rxjs/internal/operators/timestamp";
 import { NgbModal, ModalDismissReasons } from "@ng-bootstrap/ng-bootstrap";
+import { Router } from "@angular/router";
 
 @Component({
   selector: "app-doctor-channeling",
@@ -11,14 +13,17 @@ import { NgbModal, ModalDismissReasons } from "@ng-bootstrap/ng-bootstrap";
   styleUrls: ["./doctor-channeling.component.css"],
 })
 export class DoctorChannelingComponent implements OnInit {
-  result: any;
+  result1: any;
   userId: any;
   app: any;
   result2: any;
   closeResult: string;
   appointmentId: string;
-  //tt: any;
+  data2: any;
+  res: any;
   
+  //tt: any;
+
   // doctorId : string;
 
   constructor(private db: AngularFirestore, private modalService: NgbModal) {}
@@ -28,29 +33,18 @@ export class DoctorChannelingComponent implements OnInit {
     this.db
       .collection("Appointments")
       .valueChanges()
-      .subscribe((output) => {
-        // var ts = output["doctorName"];
-        // this.tt = ts;
-        // console.log(ts);
+      .subscribe(async (output) => {
 
-        this.result = output;
+        var appointmentId = output["appointmentID"]
+        console.log(appointmentId);
+
+        this.result1 = output;
         // console.log(this.result);
         // console.log(this.result["appointmentDate"]);
-        this.test(this.result);
-        this.getUserState();
-        this.getPrescriptions();
-      
+        this.test(this.result1);
+        this.testMaheema(this.app);
       });
-  }
 
-  getUserState() {
-    //   this.userId = localStorage.getItem("uid");
-    //   this.db.collection("Users").doc(this.userId).valueChanges()
-    //   .subscribe(res=>{
-    //     // console.log(res)
-    //     this.doctorId = res["doctorID"];
-    //     console.log(this.doctorId)
-    // })
   }
 
   test(data) {
@@ -61,62 +55,70 @@ export class DoctorChannelingComponent implements OnInit {
       .valueChanges()
       .subscribe((res) => {
         // console.log(res)
-        var doctorId = res["doctorID"];
+        var doctorId = res["doctorID"]
         console.log(doctorId);
-        
+
         var grouped = _.mapValues(_.groupBy(data, "doctorID"), (clist) =>
           clist.map((data) => _.omit(data, "doctorID"))
         );
         this.app = grouped[doctorId];
         console.log(this.app);
-
-        // var ts = this.app["appointmentDate"];
-        // console.log(ts);
+        //   console.log(maheema)
+        // const messageRef = this.db.collection('Appointments').doc('2020-10-05_d0000001_p0000002_2')
+        // .collection('Prescriptions').doc('uZUBs40iQr31dB6F9Ysi').get().subscribe(maheema=>{
+        // })
+        // console.log(messageRef);
         
+        // var ts = this.app["appointmentDate"];
+        console.log(this.data2);
       });
   }
 
+  testMaheema(app) {
+    const data2 = this.db.collection('Appointments').doc('2020-10-05_d0000001_p0000002_2').collection("Prescriptions").valueChanges()
+    .subscribe(output2 => {
+      this.res = output2;
+      console.log(output2);
+
+    })       
+    // console.log(data2)
+  }
+
   open(content) {
-    this.modalService.open(content, {ariaLabelledBy: 'modal-basic-title'}).result.then((result) => {
-      this.closeResult = `Closed with: ${result}`;
-    }, (reason) => {
-      this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
-    });
+    this.modalService
+      .open(content, { ariaLabelledBy: "modal-basic-title" })
+      .result.then(
+        (result) => {
+          this.closeResult = `Closed with: ${result}`;
+        },
+        (reason) => {
+          this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
+        }
+      );
   }
 
   open2(content2) {
-    this.modalService.open(content2, {ariaLabelledBy: 'modal-basic-title'}).result.then((result) => {
-      this.closeResult = `Closed with: ${result}`;
-    }, (reason) => {
-      this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
-    });
+    this.modalService
+      .open(content2, { ariaLabelledBy: "modal-basic-title" })
+      .result.then(
+        (result) => {
+          this.closeResult = `Closed with: ${result}`;
+        },
+        (reason) => {
+          this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
+        }
+      );
   }
 
   private getDismissReason(reason: any): string {
     if (reason === ModalDismissReasons.ESC) {
-      return 'by pressing ESC';
+      return "by pressing ESC";
     } else if (reason === ModalDismissReasons.BACKDROP_CLICK) {
-      return 'by clicking on a backdrop';
+      return "by clicking on a backdrop";
     } else {
       return `with: ${reason}`;
     }
+  }
 
-}
-
-getPrescriptions(){
-  this.appointmentId = localStorage.getItem("appointmentID");
-  console.log(this.appointmentId);
-
-  // this.db
-  //     .collection("Appointments")
-  //     .doc(this.appointmentId)
-  //     .collection("Prescriptions")
-  //     .valueChanges()
-  //     .subscribe((output) => { 
-  //       this.result2 = output;
-  //       console.log(this.result2);
-  //     });
-
-}
-
+  
 }
